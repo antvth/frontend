62ef581d8c8627b5177009aacc32bcec82406348
antvtph <antvtph22740@fpt.edu.vn>
2023-01-31 14:36:15 +0700

    update

--- bai5-angular/add.js
+++ bai5-angular/add.js
@@ -2,7 +2,17 @@
 function myDemo($scope) {
     $scope.welcome = "hello world";
 }
-var add = angular.module("myapp", []);
-add.controller("demoController", myDemo);
 
+function myDemo2($scope) {
+    $scope.HoTen = "AN";
+    $scope.Tuoi = "19";
+    $scope.Email = "antvtph22740";
+}
+function myinfo($scope) {
+    $scope.huhu = "chu mung nam moi";
+}
+var app = angular.module("myapp", []);
+app.controller("demoController", myDemo);
+app.controller("demo2", myDemo2);
+app.controller("myinfoController", myinfo);
 
